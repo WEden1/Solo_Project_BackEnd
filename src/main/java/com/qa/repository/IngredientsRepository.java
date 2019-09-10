f5ffa18008091d2e4a7b396b8fd70fdc417bd7d2
William <william.eden@academytrainee.com>
2019-09-10 13:32:20 +0100

    added a test for the app class created homeControllerTest

--- src/main/java/com/qa/repository/IngredientsRepository.java
+++ src/main/java/com/qa/repository/IngredientsRepository.java
@@ -4,7 +4,4 @@ import com.qa.models.Ingredients;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IngredientsRepository extends JpaRepository<Ingredients, Long>{
-
-
-
 }
